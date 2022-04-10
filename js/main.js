b30ae4e43695cc6aa3b6f4bdb2691c1df5f04693
Zudikhin <Zudikhin>
2022-04-10 11:41:18 +0300

    pavki 1

--- js/main.js
+++ js/main.js
@@ -2,19 +2,36 @@ $(document).ready(function () {
     "use strict";
 
     $(".menu_cards").click(function() {
-        $(".header_modal").removeClass('active');
+        $(".dropdown_menu").removeClass('active');
         $([document.documentElement, document.body]).animate({
             scrollTop: $(".cards").offset().top
         }, 1000);
     });
 
     $(".menu_dive").click(function() {
-        $(".header_modal").removeClass('active');
+        $(".dropdown_menu").removeClass('active');
         $([document.documentElement, document.body]).animate({
             scrollTop: $(".dive").offset().top - 62
         }, 1000);
     });
+
+    $(".dropdown_menu").click(function() {
+        $(".dropdown_menu").removeClass('active');
+    });
+
+    $(".header_content_mobile_logo").click(function() {
+        $(".dropdown_menu").addClass("active");
+    });
+
+    $(".dropdown_menu_close").click(function() {
+        $(".dropdown_menu").removeClass("active");
+    });
     
+    $(".cards_cost").click(function() {
+        $(".marquiz__button").trigger("click");
+    });
+
+
 
     $.fn.isInViewport = function() {
         var elementTop = $(this).offset().top;
